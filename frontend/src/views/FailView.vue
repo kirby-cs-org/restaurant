0b34dc8d40c0bb50447a0b4b5396a625fbc6f996
XDbanana <koonx6520@outlook.com>
2024-10-05 21:50:43 +0700

    remove test page (fail success)

--- frontend/src/views/FailView.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1 class="text-red-800 font-bold">Payment Failed</h1>
-</template>
--- frontend/src/views/SuccessView.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1 class="text-green-800 font-bold">Payment Success</h1>
-</template>
