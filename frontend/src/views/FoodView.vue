859b0fb8b302573ae76951be0c32df64408e3bf9
xssxx <koonx6520@gmail.com>
2024-10-10 20:35:44 +0700

    customer cant see +Food

--- frontend/src/views/FoodView.vue
+++ frontend/src/views/FoodView.vue
@@ -6,9 +6,11 @@ import Search from '@/components/Search.vue'
 import { foodsStore } from '@/stores/cart'
 import Cart from '@/components/Cart.vue'
 import router from '@/router'
+import userApi from '@/api/userApi'
 
 const foodStore = foodsStore()
 const searchQuery = ref('')
+const role = ref('')
 
 const fetchFoods = async () => {
     await foodStore.fetchFoods()
@@ -21,11 +23,17 @@ const filteredFoods = computed(() => {
     )
 })
 
+const getUser = async () => {
+    const { data: res } = await userApi.getUserByJwt()
+    role.value = res.data.role
+}
+
 const addfood = () => {
     router.push('/addfood')
 }
 onMounted(() => {
     fetchFoods()
+    getUser()
 })
 </script>
 
@@ -51,6 +59,7 @@ onMounted(() => {
             </section>
             <section>
                 <div
+                    v-if="role === 'ADMIN'"
                     class="inline py-2 px-4 rounded-md items-center cursor-pointer bg-yellow-300 shadow-md"
                 >
                     <span @click="addfood"><fa icon="add" /> Food </span>
